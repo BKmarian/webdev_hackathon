7bdf9d079c76fd094accb9233948ccf0f877a585
roots4 <alexandru424@gmail.com>
2015-12-12 19:28:06 +0200

    logout jobs

--- project/app/controllers/data_controller.rb
+++ project/app/controllers/data_controller.rb
@@ -1,8 +1,14 @@
 class DataController < ActionController::Base
 
 	def jobs
-		@availaible_jobs = Job.all.select{ |job| (Application.find_by( :job_id => job.id ) != nil) ? 
-			(Application.find_by( :job_id => job.id ).student_id != Student.find_by( :email => session[:user_account] ).id) : true }
+
+		if session[:user_account] == nil
+			@availaible_jobs = Job.all
+		else
+			@availaible_jobs = Job.all.select{ |job| (Application.find_by( :job_id => job.id ) != nil) ? 
+				(Application.find_by( :job_id => job.id ).student_id != Student.find_by( :email => session[:user_account] ).id) : true }
+		end
+		
 		@jobs_ids = @availaible_jobs.map(&:id)
 		@jobs_titles = @availaible_jobs.map(&:title)
 		@companies_names = @availaible_jobs.map{ |this_job| ( (this_job.company != nil) ? this_job.company.name : nil) }
